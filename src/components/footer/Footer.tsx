10834606018e9e7a2f0a625cfc720b4a10173adc
System Administrator <root@Caos-MacBook-Pro.local>
2023-04-11 23:02:54 +0900

    Latest

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="w-full absolute bottom-1 text-gray-500 text-center font-calibri text-[10.5px]">
+            Copyright 2022 JEMS Inc.  All rights reserved
+        </div>
+    )
+}
+
+export default Footer
